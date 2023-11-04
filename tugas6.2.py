d5243e74f47ce1b642609f55ca6b3fbd32ad4ee7
Alifsurya <146050995+Alifsurya@users.noreply.github.com>
2023-11-04 10:51:01 +0000

    Add files via upload

---
+++ tugas6.2.py
@@ -0,0 +1,10 @@
+total = 0
+for i in range (1,20,2):
+    if i < 19:
+        print(i ,end="+")   
+    else:
+        print(i ,end="=")
+    total = total + i
+print(total)
+
+    
---
+++ tugas6.3.py
@@ -0,0 +1,5 @@
+baris = int(input("Masukkan angka: "))
+
+for i in range(1, baris+1):
+    print("*"*i)
+
